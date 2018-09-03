5e108a2ec06424b6b849e521d980f4826e032064
JohnnysRibeiro <johnnys.ribeiro@gmail.com>
2018-09-03 20:12:55 -0300

    removing route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   apipie
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'users', to: "users#index", as: "all_users"
   get 'user/:id', to: "users#show", as: "user"
   post 'user', to: "users#create", as: "create_user"
   put 'user/:id', to: "users#update", as: "update_user"
